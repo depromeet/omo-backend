a78921fec65280720fe533503bfe94952e03b642
이승윤 <yyuunn17@naver.com>
2021-09-18 16:18:04 +0900

    [INIT]

---
+++ src/main/java/com/depromeet/omobackend/OmoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.depromeet.omobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/depromeet/omobackend/OmoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depromeet.omobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
