4a3adeb4451f4994e9cc22aa1bdd48d967e92b4f
이승윤 <yyuunn17@naver.com>
2021-09-23 00:54:47 +0900

    [ADD] JPA method

--- src/main/java/com/depromeet/omobackend/repository/user/UserRepository.java
+++ src/main/java/com/depromeet/omobackend/repository/user/UserRepository.java
@@ -3,5 +3,8 @@ package com.depromeet.omobackend.repository.user;
 import com.depromeet.omobackend.domain.user.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
