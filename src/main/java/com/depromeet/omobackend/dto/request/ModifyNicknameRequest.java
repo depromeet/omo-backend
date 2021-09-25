1bcbe8712713cfb3d144fcd8e712ff4be44ae885
이승윤 <yyuunn17@naver.com>
2021-09-25 17:16:03 +0900

    [UPDATE] validation 검증 추가

--- src/main/java/com/depromeet/omobackend/dto/request/ModifyNicknameRequest.java
+++ src/main/java/com/depromeet/omobackend/dto/request/ModifyNicknameRequest.java
@@ -4,15 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ModifyNicknameRequest {
 
-    @Max(20)
+    @Size(min = 2, max = 8)
     @NotBlank
     private String nickname;
 
