35784c0beda8b98c6a04f4ac335e93ce11d7d2c2
이승윤 <yyuunn17@naver.com>
2021-09-26 22:46:50 +0900

    [UPDATE] entity

--- src/main/java/com/depromeet/omobackend/domain/omakase/Omakase.java
+++ src/main/java/com/depromeet/omobackend/domain/omakase/Omakase.java
@@ -2,7 +2,6 @@ package com.depromeet.omobackend.domain.omakase;
 
 import com.depromeet.omobackend.domain.bookmark.Bookmark;
 import com.depromeet.omobackend.domain.location.Location;
-import com.depromeet.omobackend.domain.menu.Menu;
 import com.depromeet.omobackend.domain.stamp.Stamp;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,13 +26,13 @@ public class Omakase {
 
     private String address;
 
-    private String city;
-
     private String country;
 
     private String phoneNumber;
 
     private String photoUrl;
+
+    private String description;
 
     @Size(max = 20)
     @Enumerated(EnumType.STRING)
@@ -52,18 +51,15 @@ public class Omakase {
     @OneToMany(mappedBy = "omakase", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
     private List<Bookmark> bookmarks;
 
-    @OneToMany(mappedBy = "omakase", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
-    private List<Menu> menus;
-
     @OneToOne(mappedBy = "omakase", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
     private Location location;
 
     @Builder
-    public Omakase(String name, String address, String city, String country, String phoneNumber, String photoUrl, Category category, Time openTime, Time closeTime, Holiday holiday) {
+    public Omakase(String name, String address, String description, String country, String phoneNumber, String photoUrl, Category category, Time openTime, Time closeTime, Holiday holiday) {
         this.name = name;
         this.address = address;
-        this.city = city;
         this.country = country;
+        this.description = description;
         this.phoneNumber = phoneNumber;
         this.photoUrl = photoUrl;
         this.category = category;
