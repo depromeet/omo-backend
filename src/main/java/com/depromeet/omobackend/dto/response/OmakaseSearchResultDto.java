e831ef949392946aee232bddd5666fdcede724ce
이승윤 <yyuunn17@naver.com>
2021-09-26 23:06:00 +0900

    [FEAT] 오마카세 검색 api 구현 - 변동 가능

---
+++ src/main/java/com/depromeet/omobackend/dto/response/OmakaseSearchResultDto.java
@@ -0,0 +1,24 @@
+package com.depromeet.omobackend.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OmakaseSearchResultDto {
+
+    private Long id;
+
+    private String name;
+
+    private String country;
+
+    private String imageUrl;
+
+    private String description;
+
+}
