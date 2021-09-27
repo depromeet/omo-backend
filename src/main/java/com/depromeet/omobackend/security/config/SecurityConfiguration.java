a0f74dd8cd49dd53ad07c2080aaf3327ab182345
waristo <waristo.dev@gmail.com>
2021-09-27 13:28:19 +0900

    [UPDATE] oauth2 로그인 설정 추가

--- src/main/java/com/depromeet/omobackend/security/config/SecurityConfiguration.java
+++ src/main/java/com/depromeet/omobackend/security/config/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.depromeet.omobackend.security.config;
 
+import com.depromeet.omobackend.security.oauth.OmoOauthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,6 +11,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+    @Autowired
+    private OmoOauthService omoOauthService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,7 +23,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter implemen
                 .authorizeRequests()
                     .antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
                     .anyRequest().authenticated()
-                .and();
+                .and()
+                .oauth2Login()
+                    .userInfoEndpoint()
+                    .userService(omoOauthService)
+                    .and()
+//                    .successHandler()
+//                    .failureHandler()
+                    .permitAll()
+        ;
     }
 
     @Override
