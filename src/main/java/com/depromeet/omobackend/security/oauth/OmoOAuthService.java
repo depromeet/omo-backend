434f72a6daeeac2258ccdce7b8329cbc80f5fd92
waristo <waristo.dev@gmail.com>
2021-09-29 16:43:36 +0900

    [FEAT] OAuth 로그인 및 회원가입

---
+++ src/main/java/com/depromeet/omobackend/security/oauth/OmoOAuthService.java
@@ -0,0 +1,53 @@
+package com.depromeet.omobackend.security.oauth;
+
+import com.depromeet.omobackend.domain.user.User;
+import com.depromeet.omobackend.repository.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Optional;
+
+@Service
+public class OmoOAuthService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest oAuth2userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService oAuth2UserService = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = oAuth2UserService.loadUser(oAuth2userRequest);
+
+        String registrationId = oAuth2userRequest.getClientRegistration().getRegistrationId();
+        String userNameAttributeName = oAuth2userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+
+        save(attributes);
+
+        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))
+                , attributes.getAttributes()
+                , attributes.getNameAttributeKey());
+    }
+    private User save(OAuthAttributes attributes) {
+        String email = attributes.getEmail();
+        Optional<User> optionalUser = userRepository.findByEmail(email);
+        if (optionalUser.isPresent()) return optionalUser.get();
+        else {
+            User user = User.builder()
+                    .nickname("a")
+                    .email(email)
+                    .description(null)
+                    .profileImage(null)
+                    .build();
+            return userRepository.save(user);
+        }
+    }
+}
