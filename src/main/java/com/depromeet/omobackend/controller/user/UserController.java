a40ca8a93a69e20461f37e8f49f12bfdca4c0f3b
yunyoung1819 <yunyoung1819@hanmail.net>
2021-09-30 02:00:34 +0900

    [ADD] OAuth2 로그인 테스트 페이지 추가

--- src/main/java/com/depromeet/omobackend/controller/user/UserController.java
+++ src/main/java/com/depromeet/omobackend/controller/user/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 public class UserController {
-
     private final UserService userService;
 
     @PostMapping(value = "/api/user")
