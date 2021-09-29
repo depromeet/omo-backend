6d8b719096f5795d561a39d65003dbb6f322783c
waristo <waristo.dev@gmail.com>
2021-09-29 16:44:10 +0900

    [ADD] OAuth 정보 DTO

---
+++ src/main/java/com/depromeet/omobackend/security/oauth/OAuthAttributes.java
@@ -0,0 +1,49 @@
+package com.depromeet.omobackend.security.oauth;
+
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String email;
+
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String email) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.email = email;
+    }
+
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if (registrationId.equals("kakao")) {
+            return ofKakao(userNameAttributeName, attributes);
+        } else if (registrationId.equals("naver")) {
+            return ofNaver(userNameAttributeName,attributes);
+        }
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> kakao_account = (Map<String, Object>) attributes.get("kakao_account");
+
+        return new OAuthAttributes(attributes,
+                userNameAttributeName,
+                (String) kakao_account.get("email"));
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return new OAuthAttributes(attributes,
+                userNameAttributeName,
+                (String) response.get("email"));
+    }
+
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return new OAuthAttributes(attributes,
+                userNameAttributeName,
+                (String) attributes.get("email"));
+    }
+}
