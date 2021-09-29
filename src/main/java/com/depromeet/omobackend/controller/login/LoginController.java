28a0f84c09fc248b52ccf3b46cfcf4c364a7f868
yunyoung1819 <yunyoung1819@hanmail.net>
2021-09-30 01:56:40 +0900

    [ADD] OAuth2 로그인 테스트 페이지 추가

--- src/main/java/com/depromeet/omobackend/controller/login/LoginController.java
+++ src/main/java/com/depromeet/omobackend/controller/login/LoginController.java
@@ -1,9 +1,9 @@
 package com.depromeet.omobackend.controller.login;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class LoginController {
 
     @GetMapping(value="/oauth2Login")
