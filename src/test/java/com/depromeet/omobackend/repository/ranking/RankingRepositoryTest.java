4c866b4abd5e9dcc6d1e082a633e3bb6b8c13e34
waristo <waristo.dev@gmail.com>
2021-10-07 01:11:14 +0900

    [ADD] 랭킹 레포지토리 테스트

---
+++ src/test/java/com/depromeet/omobackend/repository/ranking/RankingRepositoryTest.java
@@ -0,0 +1,21 @@
+package com.depromeet.omobackend.repository.ranking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RankingRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getRankers() {
+    }
+
+    @Test
+    void getRankersMoreThanUserStamp() {
+    }
+}
