550a36b97e08bb1d9be4358e24ea393337040f02
waristo <waristo.dev@gmail.com>
2021-10-07 01:10:33 +0900

    [FEAT] Ranking Controller

---
+++ src/main/java/com/depromeet/omobackend/controller/RankingController.java
@@ -0,0 +1,32 @@
+package com.depromeet.omobackend.controller;
+
+import com.depromeet.omobackend.dto.response.RankingDto;
+import com.depromeet.omobackend.dto.response.RankingInCountyDto;
+import com.depromeet.omobackend.service.ranking.RankingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class RankingController {
+    private final RankingService rankingService;
+
+    @GetMapping(path = "/my-ranking")
+    public RankingDto getMyRanking() {
+        return rankingService.getMyRanking();
+    }
+
+    @GetMapping(path = "/rankers")
+    public List<RankingDto> getRankers(@RequestParam(defaultValue = "10") Long limit){
+        return rankingService.getRankers(limit);
+    }
+
+    @GetMapping(path = "/my-county-ranking")
+    public RankingInCountyDto getMyCountyRanking() {
+        return rankingService.getMyCountyRanking();
+    }
+}
