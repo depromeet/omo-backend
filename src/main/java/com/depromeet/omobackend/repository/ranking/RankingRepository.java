c016a4bc6578969633c6d34dfc16cd2222a251cc
waristo <waristo.dev@gmail.com>
2021-10-08 09:06:51 +0900

    [UPDATE] 랭킹 쿼리 변경

--- src/main/java/com/depromeet/omobackend/repository/ranking/RankingRepository.java
+++ src/main/java/com/depromeet/omobackend/repository/ranking/RankingRepository.java
@@ -8,9 +8,12 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface RankingRepository extends CrudRepository<User, Long> {
-    @Query("select u from User u join Stamp s where s.isCertified = true group by u order by count(s) desc")
+    @Query("select u from User u join Stamp s where s.isCertified = true group by u order by count(u) desc")
     List<User> getRankers(Pageable pageable);
 
-    @Query("select u from User u join Stamp s where s.isCertified = true group by u having count(s) > ?1")
+    @Query("select u from User u join Stamp s where s.isCertified = true group by u having count(u) > ?1")
     List<User> getRankersMoreThanUserStamp(Long stampCount);
+
+//    @Query("select u from User u join Stamp s where s.isCertified = true group by u having count(u) > ?1 and u.county = ?2")
+//    List<User> getRankersMoreThanUserStampAndSameCounty(Long stampCount, String county);
 }
