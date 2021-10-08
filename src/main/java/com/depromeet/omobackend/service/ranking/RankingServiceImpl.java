ce5bd4116eb154e27fb01462c880ff0b0a841e40
waristo <waristo.dev@gmail.com>
2021-10-08 09:06:21 +0900

    [FEAT] 내 랭킹, 랭커 보기 서비스 구현

--- src/main/java/com/depromeet/omobackend/service/ranking/RankingServiceImpl.java
+++ src/main/java/com/depromeet/omobackend/service/ranking/RankingServiceImpl.java
@@ -5,11 +5,12 @@ import com.depromeet.omobackend.dto.response.RankingDto;
 import com.depromeet.omobackend.dto.response.RankingInCountyDto;
 import com.depromeet.omobackend.repository.ranking.RankingRepository;
 import com.depromeet.omobackend.repository.stamp.StampRepository;
-import com.depromeet.omobackend.service.ranking.RankingService;
 import com.depromeet.omobackend.util.AuthenticationUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -22,12 +23,29 @@ public class RankingServiceImpl implements RankingService {
     @Override
     public RankingDto getMyRanking() {
         User user = authenticationUtil.getUser();
-        return null;
+        long stampCount = stampRepository.findAllByUserAndIsCertifiedTrue(user).size();
+        RankingDto rankingDto = RankingDto.builder()
+                .ranking((long) rankingRepository.getRankersMoreThanUserStamp(stampCount).size() + 1)
+                .nickname(user.getNickname())
+                .stampCount(stampCount)
+                .build();
+        return rankingDto;
     }
 
     @Override
-    public List<RankingDto> getRankers(Long limit) {
-        return null;
+    public List<RankingDto> getRankers(int limit) {
+        List<User> userList = rankingRepository.getRankers(PageRequest.of(0, limit));
+        List<RankingDto> rankers = new LinkedList<>();
+        for (int i = 1; i < userList.size(); i++){
+            rankers.add(
+                RankingDto.builder()
+                        .ranking((long)i)
+                        .nickname(userList.get(i).getNickname())
+                        .stampCount((long) stampRepository.findAllByUserAndIsCertifiedTrue(userList.get(i)).size())
+                        .build()
+            );
+        }
+        return rankers;
     }
 
     @Override
