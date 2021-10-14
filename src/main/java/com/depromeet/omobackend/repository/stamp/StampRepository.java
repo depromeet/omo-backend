6fd95783518be39ea0f409120f2ee0962a614ea3
이승윤 <yyuunn17@naver.com>
2021-10-14 16:49:52 +0900

    [UPDATE] 마이페이지에서 겹치지 않게 보여주기

--- src/main/java/com/depromeet/omobackend/repository/stamp/StampRepository.java
+++ src/main/java/com/depromeet/omobackend/repository/stamp/StampRepository.java
@@ -3,6 +3,7 @@ package com.depromeet.omobackend.repository.stamp;
 import com.depromeet.omobackend.domain.omakase.Omakase;
 import com.depromeet.omobackend.domain.stamp.Stamp;
 import com.depromeet.omobackend.domain.user.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.Optional;
 
 public interface StampRepository extends CrudRepository<Stamp, Long> {
     List<Stamp> findAllByUserAndIsCertifiedTrue(User user);
+    @Query("select distinct s.omakase from Stamp s where s.user=?1 order by s.createdDate desc")
     List<Stamp> findByUserOrderByCreatedDateDesc(User user);
     List<Stamp> findAllByUserAndIsCertifiedTrueOrderByCreatedDate(User user);
     Optional<Stamp> findFirstByOmakaseOrderByCreatedDateDesc(Omakase omakase);
