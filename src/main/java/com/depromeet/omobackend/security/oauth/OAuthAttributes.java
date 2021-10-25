0441ca5f3ac8d2332660a9e7c2af393bfd8f3e4b
waristo <waristo.dev@gmail.com>
2021-10-25 23:34:43 +0900

    [FIX]

--- src/main/java/com/depromeet/omobackend/security/oauth/OAuthAttributes.java
+++ src/main/java/com/depromeet/omobackend/security/oauth/OAuthAttributes.java
@@ -3,34 +3,33 @@ package com.depromeet.omobackend.security.oauth;
 import com.depromeet.omobackend.exception.InvalidOAuthException;
 import lombok.Getter;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Getter
 public class OAuthAttributes {
     private Map<String, Object> attributes;
     private String nameAttributeKey;
-    private String email;
 
-    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String email) {
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey) {
         this.attributes = attributes;
         this.nameAttributeKey = nameAttributeKey;
-        this.email = email;
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) throws InvalidOAuthException {
         if (registrationId.equals("kakao")) {
             Map<String, Object> response = (Map<String, Object>) attributes.get("kakao_account");
-            attributes.put("email", response.get("email"));
-            return new OAuthAttributes(attributes,
-                    userNameAttributeName,
-                    (String) response.get("email"));
+            Map<String, Object> newAttributes = new HashMap<>(attributes);
+            newAttributes.put("email", response.get("email"));
+            return new OAuthAttributes(newAttributes,
+                    userNameAttributeName);
         }
         else if (registrationId.equals("naver")){
             Map<String, Object> response = (Map<String, Object>) attributes.get("response");
-            attributes.put("email", response.get("email"));
+            Map<String, Object> newAttributes = new HashMap<>(attributes);
+            newAttributes.put("email", response.get("email"));
             return new OAuthAttributes(attributes,
-                    userNameAttributeName,
-                    (String) response.get("email"));
+                    userNameAttributeName);
         }
         else {
             throw new InvalidOAuthException();
