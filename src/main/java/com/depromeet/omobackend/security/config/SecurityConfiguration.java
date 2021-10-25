9a5b7f4a05e4e95320832340471ead23c90a2f34
waristo <waristo.dev@gmail.com>
2021-10-25 18:47:20 +0900

    [UPDATE]

--- src/main/java/com/depromeet/omobackend/security/config/SecurityConfiguration.java
+++ src/main/java/com/depromeet/omobackend/security/config/SecurityConfiguration.java
@@ -29,6 +29,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter implemen
                     .antMatchers(HttpMethod.GET, "/user/check/**").permitAll()
                     .antMatchers("/user").permitAll()
                     .antMatchers("/profile").permitAll()
+                    .antMatchers("/oauth2/authorization/**").permitAll()
                     .antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
