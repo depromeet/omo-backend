2bb97a2a67a201b25fb3bc0f5ee491abde9a4f60
이승윤 <yyuunn17@naver.com>
2021-11-09 16:51:57 +0900

    [FIX] add @Configuration

--- src/main/java/com/depromeet/omobackend/security/config/WebConfiguration.java
+++ src/main/java/com/depromeet/omobackend/security/config/WebConfiguration.java
@@ -1,5 +1,6 @@
 package com.depromeet.omobackend.security.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+@Configuration
 public class WebConfiguration implements WebMvcConfigurer  {
 
     @Override
