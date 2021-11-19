be58216a461ac4aebc7faaa9371706002c0927b8
yunyoung1819 <yunyoung1819@hanmail.net>
2021-11-19 18:25:45 +0900

    [FIX]

--- src/main/java/com/depromeet/omobackend/security/config/SecurityConfiguration.java
+++ src/main/java/com/depromeet/omobackend/security/config/SecurityConfiguration.java
@@ -16,7 +16,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Collections;
@@ -59,11 +58,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter implemen
         http.addFilterBefore(new TokenFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
     }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/workspace/omo-backend/images/profile/**")
-                .addResourceLocations("file:///workspace/omo-backend/images/profile/");
-    }
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/workspace/omo-backend/images/profile/**")
+//                .addResourceLocations("file:///workspace/omo-backend/images/profile/");
+//    }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
