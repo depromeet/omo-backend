8edb5911715945cba7c48f008b96a26f58aad4bd
이승윤 <yyuunn17@naver.com>
2021-12-11 18:22:39 +0900

    [UPDATE]

--- src/main/java/com/depromeet/omobackend/security/admin/AdminService.java
+++ src/main/java/com/depromeet/omobackend/security/admin/AdminService.java
@@ -4,5 +4,6 @@ import com.depromeet.omobackend.dto.request.OmakaseRegistrationRequest;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface AdminService {
-    void registrationOmakase(OmakaseRegistrationRequest request, MultipartFile file);
+    long registrationOmakase(OmakaseRegistrationRequest request);
+    void registrationOmakase(Long id, MultipartFile file);
 }
--- src/main/java/com/depromeet/omobackend/security/admin/AdminServiceImpl.java
+++ src/main/java/com/depromeet/omobackend/security/admin/AdminServiceImpl.java
@@ -1,12 +1,16 @@
 package com.depromeet.omobackend.security.admin;
 
 import com.depromeet.omobackend.domain.omakase.Category;
+import com.depromeet.omobackend.domain.omakase.Holiday;
+import com.depromeet.omobackend.domain.omakase.Level;
 import com.depromeet.omobackend.domain.omakase.Omakase;
 import com.depromeet.omobackend.dto.request.OmakaseRegistrationRequest;
+import com.depromeet.omobackend.exception.OmakaseNotFoundException;
 import com.depromeet.omobackend.repository.omakase.OmakaseRepository;
 import com.depromeet.omobackend.util.ImageUploadUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
@@ -16,24 +20,31 @@ public class AdminServiceImpl implements AdminService {
     private final OmakaseRepository omakaseRepository;
 
     @Override
-    public void registrationOmakase(OmakaseRegistrationRequest request, MultipartFile file) {
-        String photoUrl = file != null ? ImageUploadUtil.uploadOmakaseImage(file) : null;
-
-        omakaseRepository.save(
+    public long registrationOmakase(OmakaseRegistrationRequest request) {
+        return omakaseRepository.save(
                 Omakase.builder()
                         .address(request.getAddress())
                         .businessHours(request.getBusinessHours())
                         .category(Category.SUSHI)
                         .county(request.getCounty())
                         .description(request.getDescription())
-                        .holiday(request.getHoliday())
-                        .level(request.getLevel())
+                        .holiday(Holiday.valueOf(request.getHoliday()))
+                        .level(Level.valueOf(request.getLevel()))
                         .name(request.getName())
                         .phoneNumber(request.getPhoneNumber())
-                        .photoUrl(photoUrl)
+                        .photoUrl(null)
                         .priceInformation(request.getPriceInformation())
                 .build()
-        );
-
+        ).getId();
     }
+
+    @Transactional
+    @Override
+    public void registrationOmakase(Long id, MultipartFile file) {
+        String photoUrl = file != null ? ImageUploadUtil.uploadOmakaseImage(file) : null;
+        Omakase omakase = omakaseRepository.findById(id)
+                .orElseThrow(OmakaseNotFoundException::new);
+        omakase.updatePhotoUrl(photoUrl);
+    }
+
 }
