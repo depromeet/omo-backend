5632ff779dd170bb55db3964ee47d1fc05de8342
이승윤 <yyuunn17@naver.com>
2021-12-11 17:12:26 +0900

    [DELETE] location entity
    저희 서비스에서 지도를 사용하지 않다보니 좌표를 저장하는 것이 어렵다고 생각했고, 사용하는 곳이 없어 지웠습니다.

--- src/main/java/com/depromeet/omobackend/repository/location/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.depromeet.omobackend.repository.location;
-
-import com.depromeet.omobackend.domain.location.Location;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LocationRepository extends CrudRepository<Location, Long> {
-}
