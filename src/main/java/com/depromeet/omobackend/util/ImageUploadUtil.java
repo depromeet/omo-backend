a607bef3ff8718139931e98ad08cc72983dee966
이승윤 <yyuunn17@naver.com>
2021-12-11 16:17:45 +0900

    [ADD] 오마카세 이미지 저장 로직

--- src/main/java/com/depromeet/omobackend/util/ImageUploadUtil.java
+++ src/main/java/com/depromeet/omobackend/util/ImageUploadUtil.java
@@ -1,7 +1,11 @@
 package com.depromeet.omobackend.util;
 
+import com.depromeet.omobackend.exception.FileIsEmptyException;
+import com.depromeet.omobackend.exception.FileUploadFailedException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.FileCopyUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,11 +16,15 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.UUID;
 
 @Slf4j
 public class ImageUploadUtil {
     public static final String MD_5 = "MD5";
     public static final String UTF_8 = "UTF-8";
+
+    @Value("omakase.image.directory")
+    private static String omakaseDirectory;
 
     public static String uploadFile(String email, String imageUploadPath, String originalFilename,
                                     byte[] fileData) throws IOException {
@@ -36,6 +44,27 @@ public class ImageUploadUtil {
         // 파일 권한 적용
         setFilePermission(target);
         return savedName;
+    }
+
+    public static String uploadOmakaseImage(MultipartFile file) {
+        if(file == null || file.isEmpty() || file.getOriginalFilename() == null) {
+            throw new FileIsEmptyException();
+        }
+
+        String extension = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+        String fileName = omakaseDirectory + "/" + UUID.randomUUID() + extension;
+
+        try {
+            File target = (File) file;
+            byte[] fileData = file.getBytes();
+            FileCopyUtils.copy(fileData, target);
+            log.debug("Files {} copy : ", fileName);
+            setFilePermission(target);
+        } catch(IOException e) {
+            throw new FileUploadFailedException();
+        }
+
+        return fileName;
     }
 
     private static void setFilePermission(File target) throws IOException {
