22307471fb4dad08880207f3090dee9db4bc81c3
이승윤 <yyuunn17@naver.com>
2021-12-11 17:27:05 +0900

    [UPDATE] 칼럼 크기 수정
    phoneNumber -> 연결 스마트콜 사용하는 경우는 12자로 보여지더라고요
    businessHours -> 네이버에 검색해서 확인했는데 너무 길게 나오더라구요..? 그래서 300자로 넉넉하게 설정했습니다.

--- src/main/java/com/depromeet/omobackend/domain/omakase/Omakase.java
+++ src/main/java/com/depromeet/omobackend/domain/omakase/Omakase.java
@@ -1,7 +1,6 @@
 package com.depromeet.omobackend.domain.omakase;
 
 import com.depromeet.omobackend.domain.recommendation.Recommendation;
-import com.depromeet.omobackend.domain.location.Location;
 import com.depromeet.omobackend.domain.stamp.Stamp;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,7 +8,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.math.BigDecimal;
 import java.util.List;
 
 @Getter
@@ -30,7 +28,7 @@ public class Omakase {
     @Column(length = 10, nullable = false)
     private String county;
 
-    @Column(length = 11)
+    @Column(length = 12)
     private String phoneNumber;
 
     private String photoUrl;
@@ -49,7 +47,7 @@ public class Omakase {
     @Column(length = 100, nullable = false)
     private String priceInformation;
 
-    @Column(length = 100, nullable = false)
+    @Column(length = 300, nullable = false)
     private String businessHours;
 
     @Column(nullable = false)
@@ -65,9 +63,6 @@ public class Omakase {
     @OneToMany(mappedBy = "omakase", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
     private List<Recommendation> recommendations;
 
-    @OneToOne(mappedBy = "omakase", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
-    private Location location;
-
     @Builder
     public Omakase(String name, String address, String county, String phoneNumber, String photoUrl, String description, Level level, Category category, String priceInformation, String businessHours, Holiday holiday) {
         this.name = name;
@@ -82,7 +77,6 @@ public class Omakase {
         this.category = category;
         this.holiday = holiday;
         this.recommendationCount = 0L;
-        this.location = new Location(BigDecimal.ONE, BigDecimal.ONE, this);
     }
 
     public Omakase plusRecommendationCount() {
@@ -93,6 +87,10 @@ public class Omakase {
     public Omakase minusRecommendationCount() {
         this.recommendationCount--;
         return this;
+    }
+
+    public void updatePhotoUrl(String url) {
+        this.photoUrl = url;
     }
 
 }
